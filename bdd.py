9d411ccf8f6b53125d1e692a1be12c50454ca657
Alec Barran <alexanderbarran@gmail.com>
2023-11-03 11:17:16 -0700

    add RR2star and even/prime node sets

--- bdd.py
+++ bdd.py
@@ -1,6 +1,15 @@
 from pyeda.boolalg.bdd import BinaryDecisionDiagram
 from pyeda.inter import *
 
+def bddvar_set(symbol: str, length: int) -> list[BDDVariable]:
+    return [bddvar(f"{symbol * 2}" + str(i)) for i in range(length)]
+
+# bad practice global vars but useful for de-duplication
+symbol_i = 'x'
+symbol_j = 'y'
+# generate lists of vars xx0, xx1, ...
+vars_i = bddvar_set(symbol_i, 5)
+vars_j = bddvar_set(symbol_j, 5)
 
 def initialize_graph() -> list[list[bool]]:
     matrix = [[False] * 32 for _ in range(32)]
@@ -14,51 +23,83 @@ def initialize_graph() -> list[list[bool]]:
 
     return matrix
 
-def node_to_expr(node_id: int, var_symbol: str) -> Expression:
+def node_to_expr(node_id: int, symbol: str) -> Expression:
     id_bin = format(node_id, 'b').rjust(5, '0')
 
     expr_strs = []
     for idx in range(5):
-        node_name = f"{var_symbol * 2}{idx}"
+        node_name = f"{symbol * 2}{idx}"
         expr_strs.append(node_name if int(id_bin[idx]) else f"~{node_name}")
 
     return expr(" & ".join(expr_strs))
 
-def graph_to_bdd(graph: list[list[bool]], start_symbol: str, end_symbol: str) -> BinaryDecisionDiagram:
+def combine_to_bdd(expr_list: list[Expression]) -> BinaryDecisionDiagram:
+    # build BDD from all expressions
+    full_expr = expr_list[0]
+    for node in expr_list[1:]:
+        full_expr |= node
+
+    return expr2bdd(full_expr)
+
+def graph_to_bdd(graph: list[list[bool]]) -> BinaryDecisionDiagram:
     edge_list = []
     for j in range(32):
         for i in range(32):
             # there is an edge from node i to node j
             if graph[j][i]:
-                node_i = node_to_expr(i, start_symbol)
-                node_j = node_to_expr(j, end_symbol)
+                node_i = node_to_expr(i, symbol_i)
+                node_j = node_to_expr(j, symbol_j)
                 edge_list.append(node_i & node_j)
 
-    # build sum of edge expressions for BDD
-    full_expr = edge_list[0]
-    for edge in edge_list[1:]:
-        full_expr |= edge
+    return combine_to_bdd(edge_list)
 
-    return expr2bdd(full_expr)
+def node_set_to_bdd(nodes: list[bool], symbol: str) -> BinaryDecisionDiagram:
+    # convert the node to an expression if it is marked True in 'nodes'
+    expr_list = [node_to_expr(node_id, symbol) for node_id in range(len(nodes)) if nodes[node_id]]
+    return combine_to_bdd(expr_list)
 
-def rr_to_rr2(rr: BinaryDecisionDiagram, symbol_i: str, symbol_j: str) -> BinaryDecisionDiagram:
-    vars_i = [bddvar(f"{symbol_i * 2}" + str(i)) for i in range(5)]
-    vars_j = [bddvar(f"{symbol_j * 2}" + str(i)) for i in range(5)]
+def add_reachability(rr1: BinaryDecisionDiagram, rr2: BinaryDecisionDiagram) -> BinaryDecisionDiagram:
     # assume 'z' is an unused symbol in rr
-    vars_z = [bddvar(f"{'z' * 2}" + str(i)) for i in range(5)]
+    vars_z = bddvar_set('z', 5)
     
-    one_step = rr.compose({vars_i[idx]: vars_z[idx] for idx in range(5)})
-    two_step = rr.compose({vars_j[idx]: vars_z[idx] for idx in range(5)})
+    # define BDDs from x to z, then z to y
+    one_step = rr1.compose({vars_i[idx]: vars_z[idx] for idx in range(5)})
+    two_step = rr2.compose({vars_j[idx]: vars_z[idx] for idx in range(5)})
 
     # combine steps and eliminate existiential quantifier
     return expr2bdd(one_step & two_step).smoothing(vars_z)
 
-def check_edge_exists(bdd: BinaryDecisionDiagram, node_i: int, symbol_i: str, node_j: int, symbol_j: str) -> bool:
+def rr_to_rr2(rr: BinaryDecisionDiagram) -> BinaryDecisionDiagram:
+    return add_reachability(rr, rr)
+
+def transitive_closure(rr: BinaryDecisionDiagram) -> BinaryDecisionDiagram:
+    bdd = rr
+    
+    # fixed point algorithm for transitive closure
+    while True:
+        prev_bdd = bdd
+        bdd = bdd | add_reachability(bdd, rr)
+
+        # bdd has same truth table after compose
+        if bdd.equivalent(prev_bdd):
+            break
+
+    return bdd
+
+def check_node_exists(bdd: BinaryDecisionDiagram, node_id: int, node_symbol: str) -> bool:
+    id_bin = format(node_id, 'b').rjust(5, '0')
+    vars = bddvar_set(node_symbol, 5)
+
+    node = {}
+    for idx in range(5):
+        node[vars[idx]] = int(id_bin[idx])
+
+    restricted = bdd.restrict(node)
+    return restricted.is_one()
+
+def check_edge_exists(bdd: BinaryDecisionDiagram, node_i: int, node_j: int) -> bool:
     i_bin = format(node_i, 'b').rjust(5, '0')
     j_bin = format(node_j, 'b').rjust(5, '0')
-    # generate lists of vars xx0, xx1, ...
-    vars_i = [bddvar(f"{symbol_i * 2}" + str(i)) for i in range(5)]
-    vars_j = [bddvar(f"{symbol_j * 2}" + str(i)) for i in range(5)]
 
     # build dictionary of edge from bddvars
     edge = {}
@@ -67,4 +108,4 @@ def check_edge_exists(bdd: BinaryDecisionDiagram, node_i: int, symbol_i: str, no
         edge[vars_j[idx]] = int(j_bin[idx])
 
     restricted = bdd.restrict(edge)
-    return not restricted.is_zero()
+    return restricted.is_one()
--- tests.py
+++ tests.py
@@ -5,43 +5,70 @@ from bdd import *
 
 class BDDTests(unittest.TestCase):
     def setUp(self):
-        self.even = [True if i % 2 == 0 else False for i in range(32)]
+        self.even_bools = [True if i % 2 == 0 else False for i in range(32)]
 
         prime_list = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31]  # known graph size
-        self.prime = [True if i in prime_list else False for i in range(32)]
+        self.prime_bools = [True if i in prime_list else False for i in range(32)]
 
         self.g = initialize_graph()
 
     def test_even(self):
-        self.assertTrue(self.even[14])
-        self.assertFalse(self.even[13])
+        even = node_set_to_bdd(self.even_bools, 'x')
+
+        # check EVEN(14) exists
+        node_exists = check_node_exists(even, 14, 'x')
+        self.assertTrue(node_exists)
+
+        # EVEN(13) does not exist
+        node_void = check_node_exists(even, 13, 'x')
+        self.assertFalse(node_void)
 
     def test_prime(self):
-        self.assertTrue(self.prime[7])
-        self.assertFalse(self.prime[2])
+        prime = node_set_to_bdd(self.prime_bools, 'y')
+
+        # check PRIME(7) exists
+        node_exists = check_node_exists(prime, 7, 'y')
+        self.assertTrue(node_exists)
+
+        # PRIME(2) does not exist
+        node_void = check_node_exists(prime, 2, 'y')
+        self.assertFalse(node_void)
 
     def test_rr(self):
-        rr = graph_to_bdd(self.g, 'x', 'y')
+        rr = graph_to_bdd(self.g)
 
         # check RR(27,3) exists
-        edge_exists = check_edge_exists(rr, 27, 'x', 3, 'y')
+        edge_exists = check_edge_exists(rr, 27, 3)
         self.assertTrue(edge_exists)
 
         # RR(27,3) does not exist
-        edge_void = check_edge_exists(rr, 16, 'x', 20, 'y')
+        edge_void = check_edge_exists(rr, 16, 20)
         self.assertFalse(edge_void)
 
     def test_rr2(self):
-        rr = graph_to_bdd(self.g, 'x', 'y')
-        rr2 = rr_to_rr2(rr, 'x', 'y')
+        rr = graph_to_bdd(self.g)
+        rr2 = rr_to_rr2(rr)
 
         # check RR2(27, 6) exists
-        edge_exists = check_edge_exists(rr2, 27, 'x', 6, 'y')
+        edge_exists = check_edge_exists(rr2, 27, 6)
         self.assertTrue(edge_exists)
 
         # RR2(27, 9) does not exist
-        edge_void = check_edge_exists(rr2, 27, 'x', 9, 'y')
+        edge_void = check_edge_exists(rr2, 27, 9)
         self.assertFalse(edge_void)
+
+    def test_rr2star(self):
+        rr = graph_to_bdd(self.g)
+        rr2 = rr_to_rr2(rr)
+        rr2star = transitive_closure(rr2)
+
+        # same edge from rr2 should still exist
+        edge_exists = check_edge_exists(rr2star, 27, 6,)
+        self.assertTrue(edge_exists)
+
+    def test_statementA(self):
+        even = node_set_to_bdd(self.even_bools, 'x')
+        prime = node_set_to_bdd(self.prime_bools, 'y')
 
 if __name__ == '__main__':
     unittest.main()
